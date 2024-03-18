99b8baf114f0dce2c7b401bdc4a2f20d2462f6d9
Leonardo Alaff <leonardoalaff@gmail.com>
2024-03-18 19:49:00 -0300

    1.3 menu bot

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const iconeMenu = document.querySelector('#icone-menu')
 const listraMenu = document.querySelector('#listra-menu')
 const menu = document.querySelector('#menu')
+const msg = document.querySelector('.msg')
+const home = document.querySelector('.home')
+const calendario = document.querySelector('.calendario')
 
-iconeMenu.addEventListener('click', () => {
-    listraMenu.classList.toggle('active')
-    menu.classList.toggle('active')
+msg.addEventListener('click', () => {
+    msg.classList.add('active')
+    home.classList.remove('active')
+    calendario.classList.remove('active')
+})
+
+home.addEventListener('click', () => {
+    msg.classList.remove('active')
+    home.classList.add('active')
+    calendario.classList.remove('active')
+})
+
+calendario.addEventListener('click', () => {
+    calendario.classList.add('active')
+    msg.classList.remove('active')
+    home.classList.remove('active')
 })
